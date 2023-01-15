e60ad8faadb8b6124f971e8706fa68eb5b9ce07c
Connor Streng <cstreng95@gmail.com>
2023-01-15 16:14:53 -0600

    Create functions for deleting vertices and edges from Graph

--- src/classes/Graph.ts
+++ src/classes/Graph.ts
@@ -18,4 +18,18 @@ export default class Graph {
     // TODO: Guard against multiple edges between the same two vertices? Could this result in a loop of sorts?
     this.edges.push(edge);
   }
+
+  public removeVertex(vertexToDelete: Vertex) {
+    this.vertices = this.vertices.filter((vertex) => vertex !== vertexToDelete);
+    this.edges = this.edges.filter(
+      (edge) => !edge.vertices.includes(vertexToDelete)
+    );
+  }
+
+  public removeEdge(edgeToDelete: Edge) {
+    this.edges = this.edges.filter((edge) => edge !== edgeToDelete);
+    this.vertices.forEach((vertex) => {
+      vertex.edges = vertex.edges.filter((edge) => edge !== edgeToDelete);
+    });
+  }
 }
