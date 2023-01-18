ea90416f97428450ee339f8bcbaf89017b947ddc
Connor Streng <cstreng95@gmail.com>
2023-01-18 07:43:57 -0600

    Make Graph a publisher

--- src/classes/Graph.ts
+++ src/classes/Graph.ts
@@ -1,22 +1,38 @@
 import Vertex from "./Vertex";
 import Edge from "./Edge";
 
+type Subscriber = (graph: Graph) => void;
+
 export default class Graph {
   public vertices: Array<Vertex>;
   public edges: Array<Edge>;
+  private subscribers: Array<Subscriber>;
 
   constructor() {
     this.vertices = [];
     this.edges = [];
+    this.subscribers = [];
+  }
+
+  public subscribe(subscriber: Subscriber) {
+    this.subscribers.push(subscriber);
+  }
+
+  private publish() {
+    this.subscribers.forEach((subscriber) => {
+      subscriber(this);
+    });
   }
 
   public addVertex(vertex: Vertex) {
     this.vertices.push(vertex);
+    this.publish();
   }
 
   public addEdge(edge: Edge) {
     // TODO: Guard against multiple edges between the same two vertices? Could this result in a loop of sorts?
     this.edges.push(edge);
+    this.publish();
   }
 
   public removeVertex(vertexToDelete: Vertex) {
@@ -24,6 +40,7 @@ export default class Graph {
     this.edges = this.edges.filter(
       (edge) => !edge.vertices.includes(vertexToDelete)
     );
+    this.publish();
   }
 
   public removeEdge(edgeToDelete: Edge) {
@@ -31,5 +48,6 @@ export default class Graph {
     this.vertices.forEach((vertex) => {
       vertex.edges = vertex.edges.filter((edge) => edge !== edgeToDelete);
     });
+    this.publish();
   }
 }
